507a193c251ffa27aecf50d10780724cfa54ee78
Aaditya kandu <aadityakandu@gamil.com>
2023-01-14 11:36:57 +0530

    Data Hiding concept

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly object = new RWOnly();
+
+        object.setName("Encapsulation solution");
+
+        System.out.println(object.getName());
+    }
   
 }
